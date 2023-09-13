d348af46e2b5d01ce64b87b0d3b47a4ae3f4c27b
hsilnam <hsilnam3@gmail.com>
2023-09-13 13:22:03 +0900

    build: 백엔드 빌드

---
+++ be/src/main/java/com/ssafy/curious/CuriousApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.curious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriousApplication.class, args);
+	}
+
+}
