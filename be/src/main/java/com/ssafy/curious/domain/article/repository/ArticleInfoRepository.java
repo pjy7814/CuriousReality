ee2a1f0b4c1c881a1828e64461304978a1c2ece6
남현실 <hsilnam3@gmail.com>
2023-09-22 17:35:01 +0900

    Revert "Merge branch 'be/develop-member' into 'master'"
    This reverts merge request !4

--- be/src/main/java/com/ssafy/curious/domain/article/repository/ArticleInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.curious.domain.article.repository;
-
-import com.ssafy.curious.domain.article.entity.ArticleInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleInfoRepository extends MongoRepository<ArticleInfo, String> {
-}
--- be/src/main/java/com/ssafy/curious/domain/model/ArticleCategory.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.curious.domain.model;
-
-public enum ArticleCategory {
-    POLITICS,
-    ECONOMICS,
-    SOCIAL,
-    SCIENCE,
-    WORLD
-}
