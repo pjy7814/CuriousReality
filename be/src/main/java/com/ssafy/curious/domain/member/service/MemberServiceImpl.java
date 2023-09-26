ebca22926b107bbe82db2283a31453e5f41d7d0c
배이경 <yikyung52@naver.com>
2023-09-26 14:45:23 +0900

    Fix : api path 수정

--- be/src/main/java/com/ssafy/curious/domain/member/service/MemberServiceImpl.java
+++ be/src/main/java/com/ssafy/curious/domain/member/service/MemberServiceImpl.java
@@ -4,12 +4,8 @@ import com.ssafy.curious.domain.member.dto.MemberDTO;
 import com.ssafy.curious.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Service
