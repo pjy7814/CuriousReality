7b2131d93e4f8c3e9ebb3b97fd7c3b308b5800e3
wooha <jwoohaha@gmail.com>
2023-09-27 13:28:52 +0900

    fix: history request dto 수정

--- be/src/main/java/com/ssafy/curious/domain/preference/dto/SaveHistoryRequest.java
+++ be/src/main/java/com/ssafy/curious/domain/preference/dto/SaveHistoryRequest.java
@@ -1,10 +1,14 @@
 package com.ssafy.curious.domain.preference.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SaveHistoryRequest {
 
     private String articleId;
