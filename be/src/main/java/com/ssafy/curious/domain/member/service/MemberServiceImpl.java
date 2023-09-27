8a8c0cad35b2b9209e5feb66c7d7b0d6f0f85920
남현실 <hsilnam3@gmail.com>
2023-09-27 13:27:28 +0900

    feat: Be/feature/article like list

--- be/src/main/java/com/ssafy/curious/domain/member/service/MemberServiceImpl.java
+++ be/src/main/java/com/ssafy/curious/domain/member/service/MemberServiceImpl.java
@@ -1,11 +1,18 @@
 package com.ssafy.curious.domain.member.service;
 
+import com.ssafy.curious.domain.article.entity.ArticleInfoEntity;
+import com.ssafy.curious.domain.article.entity.BookmarkedArticleEntity;
+import com.ssafy.curious.domain.article.repository.ArticleInfoRepository;
+import com.ssafy.curious.domain.article.repository.BookmarkedArticleRepository;
+import com.ssafy.curious.domain.member.dto.ArticleBookmarkListDTO;
 import com.ssafy.curious.domain.member.dto.MemberDTO;
 import com.ssafy.curious.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,9 +20,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
+    private final BookmarkedArticleRepository bookmarkedArticleRepository;
+    private final ArticleInfoRepository articleInfoRepository;
 
     @Override
-    public MemberDTO.Response update(MemberDTO.Request dto){
+    public MemberDTO.Response update(MemberDTO.Request dto) {
 
 //        Optional<MemberEntity> member = memberRepository.findByEmail(email);
 
@@ -23,7 +32,43 @@ public class MemberServiceImpl implements MemberService {
         return null;
     }
 
+    /**
+     * 북마크한 기사 리스트 가져오기
+     * @param dto
+     * @return
+     */
+    @Override
+    public ArticleBookmarkListDTO.Response getArticleBookmarkList(ArticleBookmarkListDTO.Request dto) {
+        String email = dto.getEmail();
 
+        List<BookmarkedArticleEntity> bookmarkedArticles = bookmarkedArticleRepository.findAllByMember_Email(email).orElse(new ArrayList<BookmarkedArticleEntity>());
+
+        ArrayList<ArticleBookmarkListDTO.Response.ArticleInfo> responseDtos = new ArrayList<>();
+
+        for (BookmarkedArticleEntity bookmarkedArticle : bookmarkedArticles) {
+            List<ArticleInfoEntity> articleInfos = articleInfoRepository.findAllByOriginalUrl(bookmarkedArticle.getUrl()).orElse(null);
+            if (articleInfos == null || articleInfos.size() == 0) {
+                continue;
+            }
+            ArticleInfoEntity articleInfo = articleInfos.get(0);
+
+            responseDtos.add(ArticleBookmarkListDTO.Response.ArticleInfo.builder()
+                    .originalUrl(articleInfo.getOriginalUrl())
+                    .category1(articleInfo.getCategory1())
+                    .category2(articleInfo.getCategory2())
+                    .title(articleInfo.getTitle())
+                    .createdAt(articleInfo.getCreatedAt())
+                    .thumbnail(articleInfo.getThumbnail())
+                    .company(articleInfo.getCompany())
+                    .article(articleInfo.getArticle())
+                    .isBookmarked(true)
+                    .build());
+        }
+
+        return ArticleBookmarkListDTO.Response.builder()
+                .articleInfos(responseDtos)
+                .build();
+    }
 
 
 }
