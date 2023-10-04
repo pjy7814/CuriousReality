d5db0826337d46e6e3992c37a28ffb3852b013d2
gogobattle@naver.com <gogobattle@naver.com>
2023-10-04 16:40:56 +0900

    feat: hotkey 처리완료, search 처리 완료

---
+++ be/src/main/java/com/ssafy/curious/domain/kakao/controller/KkoController.java
@@ -0,0 +1,33 @@
+package com.ssafy.curious.domain.kakao.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class KkoController {
+
+
+    @RequestMapping(method = RequestMethod.POST, headers = {"Accept=application/json"})
+    public String callAPI(@RequestBody Map<String, Object> params){
+
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonInString = mapper.writeValueAsString(params);
+            int x = 0;
+            System.out.println(jsonInString);
+
+            return "정상적으로 작동되었습니다";
+        } catch (Exception e) {
+            System.out.println("에러 발생 " + e);
+
+            return "에러발생";
+        }
+
+    }
+
+}
