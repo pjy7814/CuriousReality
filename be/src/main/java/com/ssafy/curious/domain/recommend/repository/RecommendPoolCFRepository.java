ce5f7a5bac8cf2be7a6feb623a60c2dbb8e62d5a
wooha <jwoohaha@gmail.com>
2023-10-05 22:25:18 +0900

    fix: 협업 필터링 기반 뉴스 추천 예외 처리

--- be/src/main/java/com/ssafy/curious/domain/recommend/repository/RecommendPoolCFRepository.java
+++ be/src/main/java/com/ssafy/curious/domain/recommend/repository/RecommendPoolCFRepository.java
@@ -4,7 +4,8 @@ import com.ssafy.curious.domain.recommend.entity.RecommendPoolCFEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface RecommendPoolCFRepository extends MongoRepository<RecommendPoolCFEntity, String> {
-    RecommendPoolCFEntity findByCreatedAtBetweenAndMemberId(LocalDateTime startTime, LocalDateTime endTime, Long memberId);
+    Optional<RecommendPoolCFEntity> findByCreatedAtBetweenAndMemberId(LocalDateTime startTime, LocalDateTime endTime, Long memberId);
 }
