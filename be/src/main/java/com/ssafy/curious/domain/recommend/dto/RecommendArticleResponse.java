0c47c1ac2a13ec775fa9152ef36bc9b48903a2d5
wooha <jwoohaha@gmail.com>
2023-10-05 14:40:42 +0900

    feat: 협업 핉터링 기반 뉴스 추천 기능 구현
    - 해당 url로 조회하였을 때 뉴스가 없을 시 예외처리

--- be/src/main/java/com/ssafy/curious/domain/recommend/dto/RecommendArticleResponse.java
+++ be/src/main/java/com/ssafy/curious/domain/recommend/dto/RecommendArticleResponse.java
@@ -1,13 +1,9 @@
 package com.ssafy.curious.domain.recommend.dto;
 
 import com.ssafy.curious.domain.article.entity.ArticleInfoEntity;
-import com.ssafy.curious.domain.model.Keyword;
-import com.ssafy.curious.domain.model.ArticlePress;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 @Getter
 public class RecommendArticleResponse {
@@ -21,19 +17,18 @@ public class RecommendArticleResponse {
     private String thumbnail;
     private String company;
     private String article;
-    private List<Keyword> keywords;
 
-    public static RecommendArticleResponse from(Optional<ArticleInfoEntity> articleInfo) {
+    public static RecommendArticleResponse from(ArticleInfoEntity articleInfo) {
         RecommendArticleResponse recommendArticleResponse = new RecommendArticleResponse();
-        recommendArticleResponse.id = articleInfo.get().getId();
-        recommendArticleResponse.originalUrl = articleInfo.get().getOriginalUrl();
-        recommendArticleResponse.category1 = articleInfo.get().getCategory1();
-        recommendArticleResponse.category2 = articleInfo.get().getCategory2();
-        recommendArticleResponse.title = articleInfo.get().getTitle();
-        recommendArticleResponse.createdAt = articleInfo.get().getCreatedAt();
-        recommendArticleResponse.thumbnail = articleInfo.get().getThumbnail();
-        recommendArticleResponse.company = articleInfo.get().getCompany();
-        recommendArticleResponse.article = articleInfo.get().getArticle();
+        recommendArticleResponse.id = articleInfo.getId();
+        recommendArticleResponse.originalUrl = articleInfo.getOriginalUrl();
+        recommendArticleResponse.category1 = articleInfo.getCategory1();
+        recommendArticleResponse.category2 = articleInfo.getCategory2();
+        recommendArticleResponse.title = articleInfo.getTitle();
+        recommendArticleResponse.createdAt = articleInfo.getCreatedAt();
+        recommendArticleResponse.thumbnail = articleInfo.getThumbnail();
+        recommendArticleResponse.company = articleInfo.getCompany();
+        recommendArticleResponse.article = articleInfo.getArticle();
 
         return recommendArticleResponse;
     }
