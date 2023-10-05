81f0527dabf0e64dd79107a54217d14055da1496
gogobattle@naver.com <gogobattle@naver.com>
2023-10-05 10:49:58 +0900

    feat: keyword required=true를 false로 바꿈

--- be/src/main/java/com/ssafy/curious/domain/kakao/controller/KkoController.java
+++ be/src/main/java/com/ssafy/curious/domain/kakao/controller/KkoController.java
@@ -1,33 +1,39 @@
 package com.ssafy.curious.domain.kakao.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/kakao")
 public class KkoController {
 
 
-    @RequestMapping(method = RequestMethod.POST, headers = {"Accept=application/json"})
-    public String callAPI(@RequestBody Map<String, Object> params){
+    @RestController
+    public class KKORestAPI {
 
-        try{
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonInString = mapper.writeValueAsString(params);
-            int x = 0;
-            System.out.println(jsonInString);
+        // 카카오톡 오픈빌더로 리턴할 스킬 API
+        @RequestMapping(method = RequestMethod.POST, headers = {"Accept=application/json"})
+        public String callAPI(@RequestBody Map<String, Object> params) {
 
-            return "정상적으로 작동되었습니다";
-        } catch (Exception e) {
-            System.out.println("에러 발생 " + e);
+            try {
+                System.out.println("여기를 들어옴");
+                ObjectMapper mapper = new ObjectMapper();
+                String jsonInString = mapper.writeValueAsString(params);
+                int x = 0;
+                System.out.println(jsonInString);
 
-            return "에러발생";
+                return "정상적으로 작동되었습니다";
+            } catch (Exception e) {
+                System.out.println("에러 발생 " + e);
+
+                return "에러발생";
+            }
+
+
         }
-
     }
-
 }
